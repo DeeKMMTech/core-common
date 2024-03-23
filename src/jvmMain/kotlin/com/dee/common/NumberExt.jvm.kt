47b7758c656701b23c41890dc99f684a44aa1ffa
Dee <hein.h@siriustech.io>
2024-03-23 21:04:55 +0700

    [Remove] Unused codes

--- src/jvmMain/kotlin/com/dee/common/NumberExt.jvm.kt
+++ src/jvmMain/kotlin/com/dee/common/NumberExt.jvm.kt
@@ -3,10 +3,9 @@ package com.dee.common
 /**
  * Created by Hein Htet
  */
+/**
+ * Created by Hein Htet
+ */
 actual fun Double.formatDecimal(digit: Int): String {
-    TODO("Not yet implemented")
-}
-
-actual fun Int.formatDuration(): String {
-    TODO("Not yet implemented")
+   return ""
 }
